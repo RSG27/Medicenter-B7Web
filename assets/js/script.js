47eb376205f3e6effe974f9e0b9c95d3fd9f23af
RSG26 <ruangaspar1383@gmail.com>
2022-05-04 11:32:01 -0300

    mudanca na linguagem do codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 window.onload = function() {
   document.querySelector(".menuMobile").addEventListener("click", function() {
-    if (document.querySelector(".menu nav ul").style.display == 'flex') {
-        document.querySelector(".menu nav ul").style.display = 'none';
+    if (document.querySelector(".menu ul").style.display == 'flex') {
+        document.querySelector(".menu ul").style.display = 'none';
     } else {
-      document.querySelector(".menu nav ul").style.display = 'flex';
+      document.querySelector(".menu ul").style.display = 'flex';
     }
   });
 };
